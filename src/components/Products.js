bdaea87ab958deb83b8725af7f1b49955e5b6a03
facufrancou <facundo.francou@gmail.com>
2022-09-05 20:57:36 -0300

    boton crear producto

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,83 +1,98 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CardGroup from "react-bootstrap/CardGroup";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import Spinner from 'react-bootstrap/Spinner';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Spinner from "react-bootstrap/Spinner";
+import ListGroup from "react-bootstrap/ListGroup";
 import { Link } from "react-router-dom";
-import '../assets/styles.css'
+import "../assets/styles.css";
 
 function Products() {
-    let [isLoad, setLoad] = useState(true);
-    let [listProducts, setListProducts] = useState([]);
+  let [isLoad, setLoad] = useState(true);
+  let [listProducts, setListProducts] = useState([]);
 
-    useEffect(() => {
-        fetch('/api/products')
-            .then(response => response.json())
-            .then((data) => {
-                setListProducts(data.products)
-                setLoad(false)
-            })
-            .catch(e => console.log(e))
-    }, [])
+  useEffect(() => {
+    fetch("/api/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setListProducts(data.products);
+        setLoad(false);
+      })
+      .catch((e) => console.log(e));
+  }, []);
 
-
-
-    if (isLoad){
-        return (
-            <div className="container col-md-1">
-                   <br/>
-                   <br/>
-                <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
-                </Spinner>
-            </div>
-          );
-    }else{
+  if (isLoad) {
     return (
-        <div className="container col-md-12">
+      <div className="container col-md-1">
+        <br />
+        <br />
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container col-md-8">
+        <br />
+        <div className="container col-md-2">
+          <Link
+            to={{
+              pathname: `/products/create`,
+            }}
+            className="link-button-create"
+          >
+            <Button variant="primary">Crear Producto</Button>
+          </Link>
+        </div>
         <br />
         <CardGroup>
-
-            <Link to={{
-                pathname: `/products/create`,
-            }} className='link-button-create'><Button variant="primary" className='button-create'>Crear</Button></Link>
-
-            <div className="products">
-                {listProducts.map((product, i) => {
-                    console.log(product.image)
-                    return (
-                        
-                        <Card className="container col-md-4" key={i} >
-                            <div id='img-product'>
-                                <Card.Img variant="top" src={"/img/products/" + product.image} /> 
-                            </div>
-                            <Card.Body>
-                                <Card.Title className='text-center'>{product.name}</Card.Title>
-                                <ListGroup className="list-group-flush">
-                                  
-                                    <ListGroup.Item>Marca: {product.brand[0]}</ListGroup.Item>
-                                    <ListGroup.Item>Precio: $ {product.price}</ListGroup.Item>
-                                    <ListGroup.Item>Descuento: {product.discount} %</ListGroup.Item>
-                                    <ListGroup.Item>Descripción: {product.description}</ListGroup.Item>
-                                    <ListGroup.Item>Eliminado: {product.deleted === true ? 'Sí' : 'No'}</ListGroup.Item>
-                                   
-                                </ListGroup>
-                                <Link to={{
-                                    pathname: `/products/${product.id}`,
-                                }}><Button variant="primary">Ver detalle</Button></Link>
-                            </Card.Body>
-                        </Card>
-                    );
-                })}
-
-            </div>
+          <div className="products">
+            {listProducts.map((product, i) => {
+              console.log(product.image);
+              return (
+                <Card className="container col-md-4" key={i}>
+                  <div id="img-product">
+                    <Card.Img
+                      variant="top"
+                      src={"/img/products/" + product.image}
+                    />
+                  </div>
+                  <Card.Body>
+                    <Card.Title className="text-center">
+                      {product.name}
+                    </Card.Title>
+                    <ListGroup className="list-group-flush">
+                      <ListGroup.Item>Marca: {product.brand[0]}</ListGroup.Item>
+                      <ListGroup.Item>Precio: $ {product.price}</ListGroup.Item>
+                      <ListGroup.Item>
+                        Descuento: {product.discount} %
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Descripción: {product.description}
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Eliminado: {product.deleted === true ? "Sí" : "No"}
+                      </ListGroup.Item>
+                    </ListGroup>
+                    <Link
+                      to={{
+                        pathname: `/products/${product.id}`,
+                      }}
+                    >
+                      <Button variant="primary">Ver detalle</Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </div>
         </CardGroup>
-        <br/>
         <br />
-        </div>
+        <br />
+      </div>
     );
-}
+  }
 }
 export default Products;
